f0cdca6fc00eb5afe6714e5e8781c1f2d590f11c
Chris <28993122+csinjin@users.noreply.github.com>
2024-09-08 19:33:09 -0700

    Add playRound() function
    Increment humanScore or computerScore based on who wins
    Output computer choice
    Output whether human won, lost, or tied

--- script.js
+++ script.js
@@ -21,5 +21,41 @@ function getHumanChoice() {
     return humanChoice
 }
 
+function playRound(humanChoice, computerChoice){
+    console.log("Computer picked: " + computerChoice)
+
+    if (humanChoice === computerChoice) {
+        humanScore++
+        computerScore++
+        console.log("You tied!")
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors"){
+        humanScore++
+        console.log("You win! Rock beats scissors!")
+    }
+    else if(humanChoice === "rock" && computerChoice === "paper"){
+        computerScore++
+        console.log("You lose! Paper beats rock!")
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++
+        console.log("You win! Paper beats rock!")
+    }
+    else if(humanChoice === "paper" && computerChoice === "scissors"){
+        computerScore++
+        console.log("You lose! Scissors beats paper!")
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        humanScore++
+        console.log("You win! Scissors beats paper!")
+    }
+    else if(humanChoice === "scissors" && computerChoice === "rock"){
+        computerScore++
+        console.log("You lose! Rock beats scissors!")
+    }
+}
+
 let humanScore = 0
 let computerScore = 0
+
+playRound(getHumanChoice(), getComputerChoice())
